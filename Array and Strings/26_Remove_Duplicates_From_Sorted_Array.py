e98cfce4993af947ff2f06db95d609a0bd9f5bb1
tusharra0 <tusharpatangemohan@gmail.com>
2025-08-25 09:41:53 -0400

    #26

---
+++ Array and Strings/26_Remove_Duplicates_From_Sorted_Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        j = 1  
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]  
+                j += 1
+        
+        return j
+
+
+if __name__ == "__main__":
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    sol = Solution()
+    k = sol.removeDuplicates(nums)
+    
